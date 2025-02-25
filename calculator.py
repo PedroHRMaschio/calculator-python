5cb924e51459e2d095492c3d7f4a677678569c46
PedroHRMaschio <pedrohenriquermaschio@gmail.com>
2025-02-24 23:11:25 -0300

    feat: add new methods of power and sqrt (not implemented on ui)

--- calculator.py
+++ calculator.py
@@ -31,3 +31,15 @@ class Calculator:
         result = a / b
         self._add_to_history(f"{a} / {b}", result)
         return result
+
+    def power(self, base, exponent):
+        result = base ** exponent
+        self._add_to_history(f"{base} ^ {exponent}", result)
+        return result
+
+    def sqrt(self, number):
+        if number < 0:
+            raise ValueError("Cannot take the square root of a negative number.")
+        result = number ** 0.5
+        self._add_to_history(f"√{number}", result)
+        return result
